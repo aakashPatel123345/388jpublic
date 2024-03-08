6f999c46af6ebecae732411686c8ea844cc0e8bd
Aakash Patel <aakash.patel0377@gmail.com>
2024-03-08 12:37:38 -0500

    p3 submitted

--- spring2024/p3/flask_app/app.py
+++ spring2024/p3/flask_app/app.py
@@ -14,9 +14,9 @@ from flask_app.model import MovieClient
 app = Flask(__name__)
 
 # TODO: you should fill out these with the appropriate values
-app.config['MONGO_URI'] = '' 
-app.config['SECRET_KEY'] = ''
-OMDB_API_KEY = '' 
+app.config['MONGO_URI'] = 'mongodb+srv://aakashpatel0377:mongoDBAakashMM@cluster0.tuubvqi.mongodb.net/cmsc388j_db?retryWrites=true&w=majority&appName=Cluster0'
+app.config['SECRET_KEY'] = 'secretkey3333'
+OMDB_API_KEY = 'def9a8ce' 
 
 # DO NOT REMOVE OR MODIFY THESE 4 LINES (required for autograder to work)
 if os.getenv('MONGO_URI'):
@@ -31,7 +31,6 @@ app.config.update(
 
 # don't change the name
 mongo = PyMongo(app)
-
 # don't change the name 
 movie_client = MovieClient(OMDB_API_KEY)
 
@@ -47,12 +46,39 @@ def index():
 
 @app.route('/search-results/<query>', methods=['GET'])
 def query_results(query):
-    return 'Query'
+    movie_list = []
+    error_msg = None
+    try:
+        movie_list = movie_client.search(query) # This is the list of movie object pertaining to the movie we searched
+    except ValueError as e:      
+        error_msg = e
+    return render_template('query_results.html', results = movie_list, error_msg = error_msg)
 
 @app.route('/movies/<movie_id>', methods=['GET', 'POST'])
 def movie_detail(movie_id):
-    return 'movie_detail'
+    movie = None
+    form = None
+    reviews = []
+    error_msg = None
+    review = {}
+    try:
+        movie = movie_client.retrieve_movie_by_id(movie_id)
+        form = MovieReviewForm()
+        if request.method == 'POST' and form.validate_on_submit():
+            review = {
+                'imdb_id': movie_id,
+                'commenter': form.name.data,
+                'content': form.text.data,
+                'date': current_time()
+            }
+        mongo.db.reviews.insert_one(review)
+        reviews = list(mongo.db.reviews.find({'imdb_id': movie_id}))
+    except ValueError as e:
+        error_msg = e
+
+    return render_template('movie_detail.html', reviews=reviews, error_msg = error_msg, form=form, movie = movie)
 
 # Not a view function, used for creating a string for the current time.
 def current_time() -> str:
     return datetime.now().strftime('%B %d, %Y at %H:%M:%S')
+
--- spring2024/p3/flask_app/forms.py
+++ spring2024/p3/flask_app/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
-# from wtforms import 
-# from wtforms.validators import
+from wtforms import StringField, SubmitField, TextAreaField, DateTimeField
+from wtforms.validators import InputRequired, Length
 
 class SearchForm(FlaskForm):
-    pass
+    search_query = StringField('movie_name', validators=[InputRequired(), Length(min=1, max=30)])
+    submit = SubmitField('Submit')
 
 class MovieReviewForm(FlaskForm):
-    pass
+    name = StringField('Name/Alias', validators=[InputRequired(), Length(min=1, max=50)])
+    text = TextAreaField('Content of Review', validators=[InputRequired(), Length(min=1, max=500)])
+    submit = SubmitField('Submit')
