32668d773648f0700acbff640b7141624e09730b
Aakash Patel <aakash.patel0377@gmail.com>
2024-03-09 02:50:12 -0500

    first commit

--- spring2024/p4/flask_app/config.py
+++ spring2024/p4/flask_app/config.py
@@ -1,3 +1,3 @@
 # Stores all configuration values
-SECRET_KEY = ''
-MONGODB_HOST = ''
+SECRET_KEY = '\xfaR\x11\xa7p\x197\xf6\xf0\xf7\x1e\xadj\xc5\xde\xc4'
+MONGODB_HOST = 'mongodb+srv://aakashpatel0377:mongoDBAakashMM@cluster0.tuubvqi.mongodb.net/cmsc388j_db?retryWrites=true&w=majority&appName=Cluster0'
